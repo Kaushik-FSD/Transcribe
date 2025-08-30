5bb3ca1702531444bb15dd1f9c6da90d2e1390fd
KAUSHIK MANDAL <rockstarkaushik1000@gmail.com>
2025-08-30 17:23:53 +0530

    Added MVC pattern

---
+++ routes/analyze.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const analyzeTextTranscript = require("../utils/analyzeTextTranscript");
+
+const router = express.Router();
+
+router.post("/generateSummary", async (req, res) => {
+  try {
+    const { inputTranscript } = req.body;
+
+    if (!inputTranscript) {
+      return res.status(400).json({
+        message: "inputTranscript key is required in the request body",
+        status: "error",
+      });
+    }
+
+    const analysedResponse = await analyzeTextTranscript(inputTranscript);
+    res.json(analysedResponse);
+
+    // res.send("Transcript received");
+  } catch (error) {
+    console.error("Error processing /api/analyze request:", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+      status: "error",
+    });
+  }
+});
+
+module.exports = router;
